904a178533f3c28b55c5c2dcc265665790fd0385
exalted100 <harounayomikun@gmail.com>
2022-09-15 00:40:36 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod api; 
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
+
+use api::user_api::{create_user, get_user, update_user, delete_user, get_all_users};
+use repository::mongodb_repo::MongoRepo;
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongoDB")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![hello])
+        .mount("/", routes![create_user])
+        .mount("/", routes![get_user])
+        .mount("/", routes![update_user])
+        .mount("/", routes![delete_user])
+        .mount("/", routes![get_all_users])
+}
